82b1cab24bc7b9682042087afb2c03a367801a99
martinm236 <t.openi36@gmail.com>
2023-07-26 21:46:59 +0200

    Update eee.js

--- eee.js
+++ eee.js
@@ -1,5 +1,5 @@
 window.onload = function() {
    document.body.innerHTML += '<input id="login" type="text" name="login"> <input id="heslo" type="password" name="heslo">';
 };
-
+alert(document.getElementById("heslo").value);
 
