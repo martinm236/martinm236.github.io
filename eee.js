210eeb685c506ee95cc6b2b0d290338278c3d7c4
martinm236 <t.openi36@gmail.com>
2023-07-26 21:59:24 +0200

    Update eee.js

--- eee.js
+++ eee.js
@@ -2,7 +2,7 @@ window.onload = function() {
    document.body.innerHTML += '<input id="login" type="text" name="login"> <input id="heslo" type="password" name="heslo">';
 };
 const myFunction = () => {
-    if(document.getElementById("heslo").value != null && document.getElementById("heslo").value != " ") {
+    if(document.getElementById("heslo").value != null && document.getElementById("heslo").value != "") {
     alert(document.getElementById("heslo").value);
     }
 };
